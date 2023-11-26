23700b9ba93820bd50dbafe61eeeac0405097e69
upendra <upendrard1995@gmail.com>
2023-11-26 13:04:34 +0530

    firebase google update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,20 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "sahand-jwt-auth.firebaseapp.com",
-  projectId: "sahand-jwt-auth",
-  storageBucket: "sahand-jwt-auth.appspot.com",
-  messagingSenderId: "882407887823",
-  appId: "1:882407887823:web:7de8f0356e93ac5322b8ba"
+  authDomain: "headacheproject-d0751.firebaseapp.com",
+  projectId: "headacheproject-d0751",
+  storageBucket: "headacheproject-d0751.appspot.com",
+  messagingSenderId: "268955034856",
+  appId: "1:268955034856:web:86bc5922552418ecc2955d"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-
-
-
+const app = initializeApp(firebaseConfig);
